486cbb4022d2d4986f7d905b7d62e72e0c2fbe10
Anthony Christen <anthony1998@bluewin.ch>
2025-08-25 18:48:25 +0200

    feat: wip AppShell

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { FC } from 'react';
-import {
-  Container,
-  Stack,
-  Typography,
-  Box,
-} from '@mui/material';
+import { Add as AddIcon } from '@mui/icons-material';
+import { Box, Container, Stack, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
-import {
-  Add as AddIcon,
-} from '@mui/icons-material';
+import { FC } from 'react';
 import { PageHeader } from './components/PageHeader';
 import { ProjectCard, ProjectData } from './components/ProjectCard';
 import { TeamCard, TeamData } from './components/TeamCard';
@@ -22,21 +15,24 @@ const App: FC = () => {
     {
       id: '1',
       name: 'Project A',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       teamCount: 2,
       lastUpdated: '21.04.2025 17:12',
     },
     {
       id: '2',
       name: 'Project B',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       teamCount: 1,
       lastUpdated: '19.04.2025 13:10',
     },
     {
       id: '3',
       name: 'Project C',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       teamCount: 3,
       lastUpdated: '15.04.2025 12:02',
     },
@@ -97,19 +93,12 @@ const App: FC = () => {
                 backgroundColor: 'background.paper',
               }}
             >
-              <Typography
-                variant="h6"
-                sx={{ mb: 3 }}
-              >
+              <Typography variant="h6" sx={{ mb: 3 }}>
                 Mes projets
               </Typography>
               <Stack spacing={2}>
                 {sampleProjects.map((project) => (
-                  <ProjectCard
-                    key={project.id}
-                    project={project}
-                    onOpen={handleProjectOpen}
-                  />
+                  <ProjectCard key={project.id} project={project} onOpen={handleProjectOpen} />
                 ))}
               </Stack>
             </Box>
@@ -126,19 +115,12 @@ const App: FC = () => {
                 backgroundColor: 'background.paper',
               }}
             >
-              <Typography
-                variant="h6"
-                sx={{ mb: 3 }}
-              >
+              <Typography variant="h6" sx={{ mb: 3 }}>
                 Mes équipes
               </Typography>
               <Stack spacing={2}>
                 {sampleTeams.map((team) => (
-                  <TeamCard
-                    key={team.id}
-                    team={team}
-                    onOpen={handleTeamOpen}
-                  />
+                  <TeamCard key={team.id} team={team} onOpen={handleTeamOpen} />
                 ))}
               </Stack>
             </Box>
@@ -160,10 +142,7 @@ const App: FC = () => {
         <Grid container spacing={3}>
           {sampleProjects.map((project) => (
             <Grid size={{ xs: 12, sm: 6, lg: 3 }} key={`grid-${project.id}`}>
-              <ProjectCard
-                project={project}
-                onOpen={handleProjectOpen}
-              />
+              <ProjectCard project={project} onOpen={handleProjectOpen} />
             </Grid>
           ))}
         </Grid>
@@ -183,10 +162,7 @@ const App: FC = () => {
         <Grid container spacing={3}>
           {sampleTeams.map((team) => (
             <Grid size={{ xs: 12, sm: 6, lg: 4 }} key={`grid-${team.id}`}>
-              <TeamCard
-                team={team}
-                onOpen={handleTeamOpen}
-              />
+              <TeamCard team={team} onOpen={handleTeamOpen} />
             </Grid>
           ))}
         </Grid>
